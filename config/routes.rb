99a25a26c9f85ad6bde66e64621c34ae1eeae4cd
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-05-25 17:13:00 +0000

    Alert  and notice messages working

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,17 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/movies", { :controller => "movies", :action => "create" })
-          
+
   # READ
   get("/movies", { :controller => "movies", :action => "index" })
-  
-  get("/movies/:path_id", { :controller => "movies", :action => "show" })
 
   get("/movies/new", { :controller => "movies", :action => "new" })
-  
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+
   # UPDATE
-  
+
   patch("/movies/:path_id", { :controller => "movies", :action => "update" })
-  
+
   # DELETE
   delete("/movies/:path_id", { :controller => "movies", :action => "destroy" })
 
